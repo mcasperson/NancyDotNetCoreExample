c7169f462ecc3ba3a99a7c9ffb38afd37faa1524
Matthew Casperson <matthewcasperson@gmail.com>
2017-05-29 06:07:05 +1000

    Add project files.

---
+++ NancyHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NancyHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
